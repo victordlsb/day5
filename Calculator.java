681ff32aa96267d452f34798678103e56b6695ca
Victor de los Santos <vis.bcn@gmail>
2012-10-15 14:30:43 +0100

    Calculator working

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator{
+
+	public int add(int num1,int num2){
+		return num1+num2;
+	}
+
+	public int substract(int num1,int num2){
+		return num1-num2;
+	}
+
+	public int multiply(int num1,int num2){
+		return num1*num2;
+	}
+
+	public double divide(int num1,int num2){
+		double d1=(int) num1;
+		double d2=(int) num2;
+		return d1/d2;
+	}
+
+	public int module(int num1,int num2){
+		return num1%num2;
+	}
+}
+
+
+
---
+++ Script.java
@@ -0,0 +1,46 @@
+public class Script {
+   public static void main(String[] args) {
+    Calculator calc = new Calculator();
+	System.out.print("Introduce the first number: ");
+	int num1=Integer.parseInt(System.console().readLine());
+	System.out.print("Introduce the second number: ");
+	int num2=Integer.parseInt(System.console().readLine());
+	System.out.println("\nSelect an option:\n");
+	System.out.println("1- Add");
+	System.out.println("2- Substract");
+	System.out.println("3- Multiply");
+	System.out.println("4- Divide");
+	System.out.println("5- Module");
+	double result=0;
+	boolean optionCorrect = false;
+	while (!optionCorrect){
+		int option = Integer.parseInt(System.console().readLine());
+		switch (option){
+			case 1:
+				result = (int) calc.add(num1,num2);
+				optionCorrect = true;
+				break;
+			case 2:
+				result = (int) calc.substract(num1,num2);
+				optionCorrect = true;
+				break;
+			case 3:
+				result = (int) calc.multiply(num1,num2);
+				optionCorrect = true;
+				break;
+			case 4:
+				result = calc.divide(num1,num2);
+				optionCorrect = true;
+				break;
+			case 5:
+				result = (int) calc.module(num1,num2);
+				optionCorrect = true;
+				break;
+			default:
+				System.out.println ("The option introduced is incorrect, please insert it again");
+				break;
+		}
+	}
+	System.out.print ("The result is: " + result);
+   }
+}
